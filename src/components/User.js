cbb3cb0f44b2e0ee420820a3cdba498916c81c0f
JES20 <dore.joel.dore@gmail.com>
2021-02-15 10:48:25 -0500

    Tweak User layout

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import React from "react";
 function User({ picture, name, id, location, email, phone }) {
   return (
     <div className="User card w-100 my-3">
-      <div className="row card-body justify-content-center align-items-center">
+      <div className="row card-body py-2 justify-content-center justify-content-sm-between align-items-center">
         <div className="col-4 col-sm-auto">
           <img
             src={picture.large}
@@ -12,11 +12,11 @@ function User({ picture, name, id, location, email, phone }) {
             alt={`${name.first} ${name.last}: ${picture.large}`}
           />
         </div>
-        <div className="col-8 col-sm-auto">
+        <div className="col-8 col-sm-auto mr-sm-auto">
           <h3>{name.first} {name.last}</h3>
           <h5 class="text-muted">ID: {id.value}</h5>
         </div>
-        <div className="col-11 col-sm-auto mt-3 border-left border-secondary">
+        <div className="col-11 col-sm-auto my-3 my-sm-0 mr-sm-auto border-left border-secondary">
           <address>
             {location.street.number} {location.street.name}
             <br />
