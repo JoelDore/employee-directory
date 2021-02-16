c0ca80fb8741593d2136253a611a0611552882db
JES20 <dore.joel.dore@gmail.com>
2021-02-16 08:14:26 -0500

    Handle filter change

--- src/components/DirectoryContainer.js
+++ src/components/DirectoryContainer.js
@@ -7,8 +7,10 @@ import Sort from "./Sort";
 import Filter from "./Filter";
 
 class DirectoryContainer extends Component {
+
   state = {
-    users: []
+    users: [],
+    filter: ""
   }
 
   /*Get some amount of users from API*/
@@ -24,24 +26,44 @@ class DirectoryContainer extends Component {
       .catch(err => console.log("getUsers Error: ", err))
   }
 
+  handleSort = (event) => {
+    /** TODO:
+     * Add onClick to <Sort>
+     * Get event.target data from select
+     * const sorted => sort allUsers arrays
+     * setState {users: sorted}
+     */
+  }
+
+  handleFilterChange = (event) => {
+    const value = event.target.value.toLowerCase();
+    this.setState({ filter: value })
+  }
+
   render() {
     return (
       <div className="container-fluid h-100 py-3 bg-info p-0">
         <Header heading="Employee Directory" />
         <Sort />
-        <Filter key={1} users={this.state.users} />
+        <Filter key={1} users={this.state.users} handleFilterChange={this.handleFilterChange} />
         <Table>
-          {this.state.users.map((user) => (
-            <User
-              key={user.login.uuid}
-              picture={user.picture}
-              name={user.name}
-              id={user.id}
-              location={user.location}
-              email={user.email}
-              phone={user.phone}
-            />
-          ))}
+          {this.state.users
+            .filter(user => {
+              return `${user.name.first} ${user.name.last}`
+                .toLowerCase()
+                .includes(this.state.filter)
+            })
+            .map((user) => (
+              <User
+                key={user.login.uuid}
+                picture={user.picture}
+                name={user.name}
+                id={user.id}
+                location={user.location}
+                email={user.email}
+                phone={user.phone}
+              />
+            ))}
         </Table>
       </div>
     );
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,23 @@
 import React from "react";
 
-function Filter({ users }) {
+function Filter({ users, handleFilterChange }) {
   return (
     <div className="container w-100 mx-auto mb-3 text-center py-1">
       <label htmlFor="filter">Filter by name: &nbsp;</label>
-      <input id="filter" list="options" placeholder="All Users" className="rounded pl-2" style={{ width: "fit-content" }} />
-      <datalist id="options" defaultValue="All Users">
+      <input
+        id="filter"
+        list="options"
+        placeholder="All Users"
+        className="rounded pl-2"
+        style={{ width: "fit-content" }}
+        onChange={handleFilterChange}
+      />
+      <datalist id="options">
         <option value="All Users" />
-        {users.map((user) => <option key={user.login.uuid} value={`${user.name.first} ${user.name.last}`} />)}
+        {users.map((user) => (
+          <option key={user.login.uuid} value={`${user.name.first} ${user.name.last}`}
+          />
+        ))}
       </datalist>
     </div>
   );
